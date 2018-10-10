26eb080c68d99396e5bf71973e190cfdcf65ef36
dallen23 <dallen23@my.wctc.edu>
2018-10-10 17:46:27 -0500

    Initial commit

---
+++ Encapsulation/src/lab1/Employee.java
@@ -0,0 +1,56 @@
+package lab1;
+
+import java.util.Date;
+
+/**
+ * Fix the code in this class to do PROPERTY encapsulation correctly. Also
+ * consider if any of the properties should be mandatory and use a 
+ * constructor to enforce that. Review the tips in the document 
+ * "EncapCheckList.pdf" if needed.
+ *
+ * @author      Jim Lombardo, WCTC Instructor
+ * @version     1.02
+ */
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private boolean metWithHr;
+    private boolean metDeptStaff;
+    private boolean reviewedDeptPolicies;
+    private boolean movedIn;
+    private String cubeId;
+    private Date orientationDate;
+
+    public Employee (String firstName,String lastName,String ssn){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ssn = ssn;
+        metWithHr = false;
+        metDeptStaff = false;
+        reviewedDeptPolicies = false;
+        movedIn = false;
+        cubeId = "";
+    }
+
+    public MetHr(){
+        metWithHr = true;
+    }
+
+    public MetStaff(){
+        metDeptStaff = true;
+    }
+
+    public ReviewedPolicies(){
+        reviewedDeptPolicies = true;
+    }
+
+    public MovedIn (){
+        movedIn = true;
+    }
+
+    public GiveCubeId(String cubeNum){ this.cubeId = cubeNum;}
+
+
+
+}
